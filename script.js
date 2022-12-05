6dab9c8a26b6100ec7402b5dc47bf200d88daaf3
shubhamtiwari08 <st1173076@gmail.com>
2022-12-05 11:57:15 +0530

    changed the output

--- script.js
+++ script.js
@@ -11,7 +11,7 @@ function compareValue(sum, luckyNumber) {
     if (sum % luckyNumber === 0) {
         result.innerHTML = "wow your b'day is lucky !"
     } else {
-        result.innerHTML = "oops! your bday is not lucky !"
+        result.innerHTML = "congrats ! you are a khanki chakka madari"
     }
 }
 
